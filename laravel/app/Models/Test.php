4383fac4cb8d5898b67e5cccce80121549d6cc1f
jingjing <3548900180@qq.com>
2020-03-24 16:53:57 +0800

    first commint

---
+++ laravel/app/Models/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/3/19/019
+ * Time: 10:12
+ */
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Test extends Model
+{
+
+}
